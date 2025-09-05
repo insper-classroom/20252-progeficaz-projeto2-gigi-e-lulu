15a2c6171a8cb2330523b4503cec2ba444e71ca2
scalcogigi <giovannabarrosscalco@gmail.com>
2025-09-05 14:25:07 -0300

    rotas para listar imóveis

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,17 +1,20 @@
 import pytest
 from app import create_app
 
+# com quem os testes serão realizados
 @pytest.fixture
-def client():
+def client(): 
     app = create_app(testing=True)
     with app.test_client() as client:
         yield client
 
+# lista imóveis - funcional
 def test_listar_imoveis_retorna_json(client):
     response = client.get("/imoveis")
     assert response.status_code == 200
     assert response.is_json
 
+# lista imóveis - não funcional
 def test_listar_imoveis_contem_atributos(client):
     response = client.get("/imoveis")
     data = response.get_json()
@@ -24,3 +27,5 @@ def test_listar_imoveis_contem_atributos(client):
         assert "tipo" in imovel
         assert "cidade" in imovel
         assert "preco" in imovel
+
+def test 
